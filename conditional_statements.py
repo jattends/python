a6ca8f0f40648efeba9f74664133f766dc3e0e1d
NithanthHarsha <hnithanth@gamil.com>
2025-07-30 01:21:23 +0530

    Added . conditional_statements.py

---
+++ conditional_statements.py
@@ -0,0 +1,13 @@
+"""
+age=int(input("enter your age"))
+if age>=18:
+    print("eligible for voting.....")
+else:
+    print("not eligilbe for voting....")
+"""
+number=int(input("enter a number"))
+if number%2==0:
+    print(number,"is even")
+else:
+    print(number,"is odd")
+
