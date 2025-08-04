2f060045d348004c19a41f2024241b527f03b13a
NithanthHarsha <hnithanth@gamil.com>
2025-08-04 11:01:34 +0530

    Added .online_class.py

--- online_class.py
+++ online_class.py
@@ -1,5 +1,5 @@
 grocery_list = ["milk", "eggs", "bread", "rice", "chicken", "cheese", "apples"]
-items_in_pantry = ["rice", "cheese", "toilet paper"]
+items_in_pantry = ["rice", "cheese", ]
 def remove_existing_items(grocery_list, pantry_items):
     return [item for item in grocery_list if item not in pantry_items]
 def add_new_items(shopping_list, new_items):
@@ -10,6 +10,6 @@ def add_new_items(shopping_list, new_items):
 shopping_list = remove_existing_items(grocery_list, items_in_pantry)
 new_items_needed = ["yogurt", "spinach"]
 shopping_list = add_new_items(shopping_list, new_items_needed)
-print(" Final Shopping List:")
+print("Final Shopping List:")
 for item in shopping_list:
     print("-", item)
