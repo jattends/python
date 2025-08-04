83dd2471fd794db337f734fc267323efbcda9a16
NithanthHarsha <hnithanth@gamil.com>
2025-08-04 11:41:11 +0530

    Added .favourite.py

---
+++ favourite.py
@@ -0,0 +1,4 @@
+name =input("enter your name :")
+age =int(input("enter your age :"))
+favourite_number = int(input("enter your favourite number :"))
+print(f"hello!{name} your are {age} years old, and your favourite number is {favourite_number}.")
